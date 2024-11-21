d7a51305629056710bc9e3150cdc42606d370188
zahranur4 <zahranurazijah23@gmail.com>
2024-11-21 15:23:00 +0700

    zahranuraweb

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+    function submitForm(event) {
+        event.preventDefault(); // Mencegah pengiriman form ke server
+        alert("Pendaftaran berhasil! Terima kasih telah mendaftar.");
+    }
+</script>
